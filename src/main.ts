09947b5d9d1b31089eddd6098224d54cc2bb86cf
cexbrayat <cedric@ninja-squad.com>
2021-07-24 17:18:42 +0200

    repro: vue i18n 9.2.0-alpha.7

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  locale: "en",
+  messages: {
+    en: {
+      app: {
+        hello: "Hello",
+      },
+    },
+  },
+});
+
+createApp(App)
+  .use(i18n)
+  .mount("#app");
